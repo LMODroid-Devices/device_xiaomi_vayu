309fb5e5c5668eade88ee441d90bf31b455b2444
LMO GerritBot <gerrit@libremobileos.com>
2023-11-09 12:17:59 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/lmodroid_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    lmodroid_vayu-user \
+    lmodroid_vayu-userdebug \
+    lmodroid_vayu-eng
